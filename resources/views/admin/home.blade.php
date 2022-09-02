2cd7fc31886463a07687f8a9aac172998370795a
JacopoNa <j.nardelli@virgilio.it>
2022-09-02 19:37:40 +0200

    first commit

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <h1>HOME PRIVATA</h1>
+@endsection
---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>HOME PUBBLICA</h1>
+@endsection
