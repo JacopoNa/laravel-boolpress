7e29987d881e19930139c05be5fdb422e04efc35
JacopoNa <j.nardelli@virgilio.it>
2022-09-06 15:19:10 +0200

    fix functions

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -42,22 +42,11 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate($this->validation());
         $form_data = $request->all();
         $new_post = new Post();
         $new_post->fill($form_data);
-
-        $potential_slug = Str::slug($new_post->title, '-'); 
-        $existing_slug_post = Post::where('slug', '=', $potential_slug)->first();
-
-        $slug_base = $potential_slug;
-        $counter = 1; 
-        while ($existing_slug_post) {
-            $potential_slug = $slug_base . $counter;
-            $existing_slug_post = Post::where('slug', '=', $potential_slug)->first();
-            $counter++;
-        }
-
-        $new_post->slug = $potential_slug;
+        $new_post->slug = $this->getSlug($new_post->title);
         $new_post->save();
         return redirect()->route('admin.posts.show', ['post' => $new_post->id]);
     }
@@ -109,5 +98,27 @@ class PostController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    protected function getSlug($title) {
+        $potential_slug = Str::slug($title, '-'); 
+        $existing_slug_post = Post::where('slug', '=', $potential_slug)->first();
+
+        $slug_base = $potential_slug;
+        $counter = 1; 
+        while ($existing_slug_post) {
+            $potential_slug = $slug_base . $counter;
+            $existing_slug_post = Post::where('slug', '=', $potential_slug)->first();
+            $counter++;
+        }
+
+        return $potential_slug;
+    }
+
+    public function validation() {
+        return [
+            'title' => 'bail|required|max:255',
+            'content' => 'bail|required|max:60000',
+        ];
     }
 }
