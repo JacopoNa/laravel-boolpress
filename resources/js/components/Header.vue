9ebde6c068e29ad506c58dad152704ca019e4135
JacopoNa <j.nardelli@virgilio.it>
2022-09-14 15:49:11 +0200

    header fixed

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -2,15 +2,15 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/">Home</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link" aria-current="page" href="/about">About</a>
-                        <a class="nav-link" href="/blog">Blog</a>
-                    </div>
+                    <ul class="navbar-nav">
+                        <li v-for="link,index in menuLinks" :key="index">
+                            <router-link :to="{name: link.name}" class="nav-link" href="/blog">{{ link.label }}</router-link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
@@ -19,6 +19,24 @@
 
 <script>
 export default {
-    name: 'Header'
+    name: 'Header',
+    data() {
+        return {
+            menuLinks: [
+                {
+                    name: 'home',
+                    label: 'Home'
+                },
+                {
+                    name: 'about',
+                    label: 'About'
+                },
+                {
+                    name: 'blog',
+                    label: 'Blog'
+                },
+            ]
+        }
+    }
 }
 </script>
