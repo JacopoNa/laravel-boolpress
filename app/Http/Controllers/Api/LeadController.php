b4704187da609ce9d6d6fad2bb9a5ad99590f082
JacopoNa <j.nardelli@virgilio.it>
2022-09-20 13:32:06 +0200

    validation contact form-data

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -5,11 +5,25 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Lead;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
     public function store(Request $request) {
         $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|max:255',
+            'message' => 'required|max:60000',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]); 
+        }
 
         $new_lead = new Lead();
         $new_lead->fill($data);
--- resources/js/pages/ContactPage.vue
+++ resources/js/pages/ContactPage.vue
@@ -11,17 +11,35 @@
             <div class="mb-3">
                 <label for="user-name" class="form-label">Nome</label>
                 <input v-model="userName" type="text" class="form-control" id="user-name">
+
+                <div v-if="errors.name">
+                    <div v-for="error,index in errors.name" :key="index" class="alert alert-danger" role="alert">
+                       {{ error }}
+                    </div>
+                </div>
             </div>
             <div class="mb-3">
                 <label for="user-mail" class="form-label">Mail</label>
                 <input v-model="userMail" type="email" class="form-control" id="user-mail">
+
+                <div v-if="errors.email">
+                    <div v-for="error,index in errors.email" :key="index" class="alert alert-danger" role="alert">
+                       {{ error }}
+                    </div>
+                </div>
             </div>
             <div class="mb-3">
                 <label for="user-message" class="form-label">Messaggio</label>
                 <textarea v-model="userMessage" class="form-control" id="user-message" rows="5"></textarea>
+
+                <div v-if="errors.message">
+                    <div v-for="error,index in errors.message" :key="index" class="alert alert-danger" role="alert">
+                       {{ error }}
+                    </div>
+                </div>
             </div>
 
-            <input type="submit" class="btn btn-primary">
+            <input type="submit" class="btn btn-primary" :disabled="sending">
         </form>
         
     </div>
@@ -35,11 +53,15 @@ export default {
             userName: '',
             userMail: '',
             userMessage: '',
-            success: false
+            success: false,
+            errors: {},
+            sending: false
         }
     },
     methods: {
         sendMessage() {
+            this.sending = true;
+
             axios.post('/api/leads', {
                 name: this.userName,
                 email: this.userMail,
@@ -52,8 +74,12 @@ export default {
                     this.userName = '';
                     this.userMail = '';
                     this.userMessage = '';
-
+                    this.errors = {};
+                } else {
+                    this.errors = response.data.errors;
                 }
+
+                this.sending = false;
             });
         }
     }
